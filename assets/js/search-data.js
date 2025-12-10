c3c76599920ffc89cf346548a78106948ec1d930
younghun-kim-dev <younghun0613@yonsei.ac.kr>
2025-12-10 05:02:29 +0000

    Deploying to gh-pages from @ younghun-kim-dev/younghun-kim-dev.github.io@139cec87768fd68079c891e983a279e3064b0c25 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-repositories",
+          title: "repositories",
+          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/repositories/";
+          },
         },{id: "nav-cv",
           title: "CV",
           description: "Curriculum Vitae of Younghun Kim",
